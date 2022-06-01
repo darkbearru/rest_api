a3af676e9232f478e9d36a0d276211832842ecce
Alexey Abramenko <a.abramenko@chita.ru>
2022-06-01 20:10:10 +0900

    Fix. Мелкие правки

--- index.php
+++ index.php
@@ -7,3 +7,15 @@ $loader = require __DIR__ . '/vendor/autoload.php';
 use Abramenko\RestApi\Application\Application;
 
 $app = new Application();
+
+$app->setDefaultRoute("routingTest");
+$app->run();
+
+
+function routingTest($variables)
+{
+    echo "Default Route is work<br />";
+    echo "<pre>";
+    print_r($variables);
+    echo "</pre>";
+}
