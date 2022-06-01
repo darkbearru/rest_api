3b1fa99ccce587208ab59b6733e00c708ad985be
Alexey Abramenko <a.abramenko@chita.ru>
2022-06-01 14:35:18 +0900

    Add. Полное покрытие тестами всего функционала Controller

--- tests/Controllers/ControllerTest.php
+++ tests/Controllers/ControllerTest.php
@@ -38,4 +38,48 @@ class ControllerTest extends TestCase
             $controller->getRoutes()
         );
     }
+    public function testCanAddRouteOnPostMethod(): void
+    {
+        $controller = new Controller("User", "/api/users");
+        $controller->routePost("/list", "UserService#listUsers");
+        $this->assertEquals(
+            [["POST", "/api/users/list", "UserService#listUsers", "userservice_listusers"]],
+            $controller->getRoutes()
+        );
+    }
+    public function testCanAddRouteOnPutMethod(): void
+    {
+        $controller = new Controller("User", "/api/users");
+        $controller->routePut("/list", "UserService#listUsers");
+        $this->assertEquals(
+            [["PUT", "/api/users/list", "UserService#listUsers", "userservice_listusers"]],
+            $controller->getRoutes()
+        );
+    }
+    public function testCanAddRouteOnDeleteMethod(): void
+    {
+        $controller = new Controller("User", "/api/users");
+        $controller->routeDelete("/list", "UserService#listUsers");
+        $this->assertEquals(
+            [["DELETE", "/api/users/list", "UserService#listUsers", "userservice_listusers"]],
+            $controller->getRoutes()
+        );
+    }
+    public function testCanAddMultipleRoutes(): void
+    {
+        $controller = new Controller("User", "/api/users");
+        $controller->routeGet("/profile", "UserService#userProfile");
+        $controller->routePost("/login", "UserService#userLogin");
+        $controller->routePost("/logout", "UserService#userLogout");
+        $controller->routePost("/registration", "UserService#userRegistration");
+        $this->assertEquals(
+            [
+                ["GET", "/api/users/profile", "UserService#userProfile", "userservice_userprofile"],
+                ["POST", "/api/users/login", "UserService#userLogin", "userservice_userlogin"],
+                ["POST", "/api/users/logout", "UserService#userLogout", "userservice_userlogout"],
+                ["POST", "/api/users/registration", "UserService#userRegistration", "userservice_userregistration"],
+            ],
+            $controller->getRoutes()
+        );
+    }
 }
