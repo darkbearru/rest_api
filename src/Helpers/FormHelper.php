03ea56b0d600ad70203952b8e1e7425a6bedc30d
Alexey Abramenko <a.abramenko@chita.ru>
2022-06-02 13:08:38 +0900

    Add. Добавлен Helper для проверки правильнсти введённых email и пароля

---
+++ src/Helpers/FormHelper.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Abramenko\RestApi\Helpers;
+
+class FormHelper
+{
+    public static function isEmail(
+        string $email,
+        string $mask = '/^[a-z\d]([-._a-z\d]+)@([a-z\d][-_a-z\d]+\.)+(com|ru|[a-z]{2,5})$/uis'
+    ): bool {
+        return preg_match($mask, $email);
+    }
+
+    public static function isPassword(
+        string $password,
+        string $mask = '/^[-_.a-zA-Z\d\@\#\$\%]{7,20}$/us'
+    ): bool {
+        return preg_match($mask, $password);
+    }
+}
