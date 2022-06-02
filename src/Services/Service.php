51ca38dfd0e4bef8dbb7e949f6fcb290bd34f6f0
Alexey Abramenko <a.abramenko@chita.ru>
2022-06-02 09:38:25 +0900

    Update. Добавление конектора к DB в абстрактный класс Service

--- src/Services/Service.php
+++ src/Services/Service.php
@@ -2,6 +2,15 @@
 
 namespace Abramenko\RestApi\Services;
 
+use PDO;
+use Abramenko\RestApi\Libs\DB;
+
 abstract class Service
 {
+    private PDO $_db;
+
+    public function __construct()
+    {
+        $this->_db = DB::getInstance();
+    }
 }
