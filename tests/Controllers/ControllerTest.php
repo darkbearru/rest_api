3c3ff9fa1727735a274a6cdee57177628765e244
Alexey Abramenko <a.abramenko@chita.ru>
2022-06-03 14:20:07 +0900

    Update. Правка параметров теста

--- tests/Controllers/ControllerTest.php
+++ tests/Controllers/ControllerTest.php
@@ -1,4 +1,5 @@
 <?php
+namespace Controllers;
 
 use PHPUnit\Framework\TestCase;
 use \Abramenko\RestApi\Controllers\Controller;
@@ -13,6 +14,7 @@ class ControllerTest extends TestCase
             (string)$controller
         );
     }
+
     public function testCanCreateControllerWithCustomNameParameter(): void
     {
         $controller = new Controller("User");
@@ -21,6 +23,7 @@ class ControllerTest extends TestCase
             (string)$controller
         );
     }
+
     public function testCanCreateControllerWithCustomRouteParameter(): void
     {
         $controller = new Controller("User", "/api/users");
@@ -29,42 +32,47 @@ class ControllerTest extends TestCase
             $controller->getRoute()
         );
     }
+
     public function testCanAddRouteOnGetMethod(): void
     {
         $controller = new Controller("User", "/api/users");
         $controller->routeGet("/list", "UserService#listUsers");
         $this->assertEquals(
-            [["GET", "/api/users/list", "UserService#listUsers", "userservice_listusers"]],
+            [["GET", "/api/users/list", "UserService#listUsers"]],
             $controller->getRoutes()
         );
     }
+
     public function testCanAddRouteOnPostMethod(): void
     {
         $controller = new Controller("User", "/api/users");
         $controller->routePost("/list", "UserService#listUsers");
         $this->assertEquals(
-            [["POST", "/api/users/list", "UserService#listUsers", "userservice_listusers"]],
+            [["POST", "/api/users/list", "UserService#listUsers"]],
             $controller->getRoutes()
         );
     }
+
     public function testCanAddRouteOnPutMethod(): void
     {
         $controller = new Controller("User", "/api/users");
         $controller->routePut("/list", "UserService#listUsers");
         $this->assertEquals(
-            [["PUT", "/api/users/list", "UserService#listUsers", "userservice_listusers"]],
+            [["PUT", "/api/users/list", "UserService#listUsers"]],
             $controller->getRoutes()
         );
     }
+
     public function testCanAddRouteOnDeleteMethod(): void
     {
         $controller = new Controller("User", "/api/users");
         $controller->routeDelete("/list", "UserService#listUsers");
         $this->assertEquals(
-            [["DELETE", "/api/users/list", "UserService#listUsers", "userservice_listusers"]],
+            [["DELETE", "/api/users/list", "UserService#listUsers"]],
             $controller->getRoutes()
         );
     }
+
     public function testCanAddMultipleRoutes(): void
     {
         $controller = new Controller("User", "/api/users");
@@ -74,10 +82,10 @@ class ControllerTest extends TestCase
         $controller->routePost("/registration", "UserService#userRegistration");
         $this->assertEquals(
             [
-                ["GET", "/api/users/profile", "UserService#userProfile", "userservice_userprofile"],
-                ["POST", "/api/users/login", "UserService#userLogin", "userservice_userlogin"],
-                ["POST", "/api/users/logout", "UserService#userLogout", "userservice_userlogout"],
-                ["POST", "/api/users/registration", "UserService#userRegistration", "userservice_userregistration"],
+                ["GET", "/api/users/profile", "UserService#userProfile"],
+                ["POST", "/api/users/login", "UserService#userLogin"],
+                ["POST", "/api/users/logout", "UserService#userLogout"],
+                ["POST", "/api/users/registration", "UserService#userRegistration"],
             ],
             $controller->getRoutes()
         );
