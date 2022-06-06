1c2fdffd981281d890f69ef4ac740badc2fcdce1
Alexey Abramenko <a.abramenko@chita.ru>
2022-06-06 10:54:25 +0900

    Update. Добавление JSON ответка в подтверждение мыла

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -34,7 +34,9 @@ class UserController extends Controller
 
         // Подтверждение пользователя
         $this->routeGet("/confirmation/[a:link]", function ($params) {
-            $this->_userService->Confirmation($params);
+            $this->responseJSON(
+                $this->_userService->Confirmation($params)
+            );
         });
     }
 }
